b2c52ae9677dd03127db61e4221760237a980ba8
Xhiro-satria <satriadian091@gmail.com>
2025-12-25 20:13:17 +0700

    update responsif navbar

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -3,16 +3,19 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 const Navigationbar = () => {
     return (
         <div>           
-            <Navbar bg="dark" variant="dark" fixed="top">
+            <Navbar bg="dark" variant="dark" fixed="top" expand="lg">
                 <Container>
                     <Navbar.Brand href="#">Film-Favorit</Navbar.Brand>
-                    <Nav>
-                        <Nav.Link href="#trending">Trending</Nav.Link>
-                        <Nav.Link href="#battleground">Battle Ground</Nav.Link>
-                        <Nav.Link href="#Superhero">Super Hero</Nav.Link>
-                        <Nav.Link href="#romance">Romance</Nav.Link>
-                        <Nav.Link href="#anime">Anime</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#trending">Trending</Nav.Link>
+                            <Nav.Link href="#battleground">Battle Ground</Nav.Link>
+                            <Nav.Link href="#Superhero">Super Hero</Nav.Link>
+                            <Nav.Link href="#romance">Romance</Nav.Link>
+                            <Nav.Link href="#anime">Anime</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
